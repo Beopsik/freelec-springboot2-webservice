8a1a9022437931f97192e71f3f0cb58da44bed98
Moon Beob Sik <tjswl756@naver.com>
2021-03-12 19:30:00 +0900

    feat: init project

---
+++ springboot프로파일/src/main/java/kr/or/connect/springbootprofile/SpringbootProfileApplication.java
@@ -0,0 +1,13 @@
+package kr.or.connect.springbootprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProfileApplication.class, args);
+    }
+
+}
