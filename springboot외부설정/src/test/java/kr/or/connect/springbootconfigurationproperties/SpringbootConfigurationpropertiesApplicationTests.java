1378a378143eaa1a027fe4e96cdb74a2e181d0de
Moon Beob Sik <tjswl756@naver.com>
2021-03-12 18:49:02 +0900

    feat: init project

---
+++ springboot외부설정/src/test/java/kr/or/connect/springbootconfigurationproperties/SpringbootConfigurationpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.connect.springbootconfigurationproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigurationpropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
