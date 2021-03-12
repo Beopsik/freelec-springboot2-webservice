984f50de24dc58e68a0592cb89fcac36d83a0d90
Moon Beob Sik <tjswl756@naver.com>
2021-03-12 16:06:26 +0900

    feat: init project

---
+++ springboot자동설정/src/main/java/com/bs/bsspringboot/BsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.bs.bsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsSpringbootApplication.class, args);
+    }
+
+}
---
+++ springboot자동설정/src/test/java/com/bs/bsspringboot/BsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.bsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
