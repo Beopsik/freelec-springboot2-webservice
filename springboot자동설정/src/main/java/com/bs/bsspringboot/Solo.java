25988c979ab76790e56864f6a9041b6bf029b993
Moon Beob Sik <tjswl756@naver.com>
2021-03-12 18:22:32 +0900

    feat: update maven

--- springboot자동설정/src/main/java/com/bs/bsspringboot/Solo.java
+++ springboot자동설정/src/main/java/com/bs/bsspringboot/Solo.java
@@ -1,2 +1,30 @@
-package com.bs.bsspringboot;public class Solo {
+package com.bs.bsspringboot;
+
+public class Solo {
+    String name;
+    int howLong;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHowLong() {
+        return howLong;
+    }
+
+    public void setHowLong(int howLong) {
+        this.howLong = howLong;
+    }
+
+    @Override
+    public String toString() {
+        return "Solo{" +
+                "name='" + name + '\'' +
+                ", howLong=" + howLong +
+                '}';
+    }
 }
--- springboot자동설정/src/main/java/com/bs/bsspringboot/SoloConfiguration.java
+++ springboot자동설정/src/main/java/com/bs/bsspringboot/SoloConfiguration.java
@@ -1,2 +1,17 @@
-package com.bs.bsspringboot;public class SoloConfiguration {
+package com.bs.bsspringboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SoloConfiguration {
+
+    @Bean
+    public Solo solo(){
+        Solo solo=new Solo();
+        solo.setName("js");
+        solo.setHowLong(5);
+
+        return solo;
+    }
 }
