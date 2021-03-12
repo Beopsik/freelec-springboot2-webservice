78c56d2b74f6dcebec59ca6b1bd796f042aaffe7
Moon Beob Sik <tjswl756@naver.com>
2021-03-12 21:34:24 +0900

    feat: init project

---
+++ springboot-springwebmvc/src/main/java/kr/or/connect/springbootspringwebmvc/SpringbootSpringwebmvcApplication.java
@@ -0,0 +1,13 @@
+package kr.or.connect.springbootspringwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringwebmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringwebmvcApplication.class, args);
+    }
+
+}
---
+++ springboot-springwebmvc/src/test/java/kr/or/connect/springbootspringwebmvc/SpringbootSpringwebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.connect.springbootspringwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringwebmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
