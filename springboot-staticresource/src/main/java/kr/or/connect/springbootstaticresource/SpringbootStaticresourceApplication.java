6d8f1d903e054fd44bdbee1e519c804454e18924
Moon Beob Sik <tjswl756@naver.com>
2021-03-12 22:56:51 +0900

    feat: init project

---
+++ springboot-staticresource/src/main/java/kr/or/connect/springbootstaticresource/SpringbootStaticresourceApplication.java
@@ -0,0 +1,13 @@
+package kr.or.connect.springbootstaticresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStaticresourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStaticresourceApplication.class, args);
+    }
+
+}
