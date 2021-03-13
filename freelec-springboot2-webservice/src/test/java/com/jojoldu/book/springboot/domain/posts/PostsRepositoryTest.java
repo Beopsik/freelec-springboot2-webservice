daa2930b356010717dc9981380c08767a3c0a1c5
Moon Beob Sik <tjswl756@naver.com>
2021-03-14 00:48:15 +0900

    featL add jpa test code

---
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup(){
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void savePost_Load(){
+        String title="테스트 게시글";
+        String content="테스트 본문";
+
+        postRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("tjswl756@naver.com")
+                .build());
+
+        List<Posts> postsList=postRepository.findAll();
+
+        Posts posts=postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
