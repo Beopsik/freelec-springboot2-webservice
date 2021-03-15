68d48a84793b3e999286538e41ac5c6c4e41cc5a
Moon Beob Sik <tjswl756@naver.com>
2021-03-15 15:05:13 +0900

    fix: change DB from H2 to MySQL

--- freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,13 +1,15 @@
 package com.jojoldu.book.springboot;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.List;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
