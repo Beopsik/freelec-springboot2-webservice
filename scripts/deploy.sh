07ba90e5d7c35305e1860bb03e3c84e09d2e2d55
Moon Beob Sik <tjswl756@naver.com>
2021-04-03 16:03:52 +0900

    fix: typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ echo ">새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME에 실행 권한 추가"
+echo "> $JAR_NAME 에 실행 권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
